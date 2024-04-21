202a681db9aaf112e447d09d34a165769e349a47
kungsik <kungsik@gmail.com>
2024-04-21 20:43:15 +0900

    added writing

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,22 +3,26 @@ document.addEventListener('DOMContentLoaded', function() {
   var btn = document.querySelector(".site-title__text--sub");
   var span = document.getElementsByClassName("close")[0];
   var title = document.querySelector(".site-title__wrapper"); // 타이틀을 선택하는 셀렉터
+  var particle = document.querySelector(".particles-js-canvas-el");
 
   btn.onclick = function() {
     modal.style.display = "block";
     title.classList.add("hidden"); // 모달이 열릴 때 타이틀 숨기기
+    particle.classList.add("hidden"); // 모달이 열릴 때 타이틀 숨기기
     loadRecentPosts();
   }
 
   span.onclick = function() {
     modal.style.display = "none";
     title.classList.remove("hidden"); // 모달이 닫힐 때 타이틀 보이기
+    particle.classList.remove("hidden"); // 모달이 닫힐 때 타이틀 보이기
   }
 
   window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
       title.classList.remove("hidden"); // 모달이 닫힐 때 타이틀 보이기
+      particle.classList.remove("hidden"); // 모달이 닫힐 때 타이틀 보이기
     }
   };
 });
